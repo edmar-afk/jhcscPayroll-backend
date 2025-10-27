45eb18376bc78c21eea2c048b8ddede5c7fc69da
edmar-afk <jaywrsnp6@gmail.com>
2025-10-27 14:47:41 +0800

    new

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Payroll, QrCode
+from .models import Payroll, QrCode, PayrollStatus
 
 
 admin.site.register(Payroll)
 admin.site.register(QrCode)
+admin.site.register(PayrollStatus)
